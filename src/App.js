83a9e2d553defea6e78f790b5183c7cab734d06e
ManonSella <Manon@manons-macbook-air.home>
2022-08-24 18:19:33 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        This project was coded by Manon Sella and is {""}
-        <a
-          href="https://github.com/ManonSella/final-react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+        <footer>
+          This project was coded by Manon Sella and is {""}
+          <a
+            href="https://github.com/ManonSella/final-react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
